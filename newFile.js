9080ac5d922ba36985ac96e010b480d01831af6d
Malith64 <cmsp0613@gmail.com>
2025-07-06 18:29:03 +0530

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,4 @@
+const { menuCloseButton, menuOpenButton } = require("./sript");
+
+//close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.Click());
